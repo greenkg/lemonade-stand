a3eb51882712f7c3c493665e1950075e73ab95c7
KA Green <greenkg@gmail.com>
2017-10-24 13:51:55 +0200

    Added ability for player to buy lemons and make lemonade.

--- game.rb
+++ game.rb
@@ -1,37 +1,47 @@
-class Inventory
+require_relative 'lib/player'
 
-  attr_accessor :lemons, :ice, :sugar
+class Game
+
+  ACTIONS = [
+    :buy_lemons, :make_lemonade
+  ]
 
   def initialize
-    @lemons = 0
-    @ice = 0
-    @sugar = 0
+    @player = Player.new
+
+    start_game
   end
 
-  def to_s
-    "Inventory: #{@lemons} lemons, #{@ice} bags of ice, #{@sugar} cups of sugar"
-  end
-
-  def number_of_lemons
-    "#{@lemons} lemons"
-  end
-
-  def add_lemons(num)
-    @lemons += num
-  end
-
-  def remove_lemons(num)
-    if @lemons >= num
-      @lemons -= num
-    else
-      puts "Not enough lemons to reduce by #{num}"
+private
+  
+    def start_game
+      while @player.has_cash?
+        @player.print_status
+        action = take_player_input
+        if ACTIONS.include? action
+          take_action(action)
+        else
+          puts ">>>> I'm sorry, I don't understand."
+          next
+        end
+      end
     end
-  end
+
+    def take_player_input
+      print "What would you like to do next? >> "
+      gets.chomp.gsub!(/ /, "_").to_sym
+    end
+
+    def take_action(action)
+      puts "Requested action is #{action}."
+      case action
+      when :buy_lemons
+        @player.buy_lemons(1)
+      when :make_lemonade
+        @player.make_lemonade(1, 1, 1)
+      end
+    end
 
 end
 
-myInventory = Inventory.new
-puts myInventory
-myInventory.add_lemons(2)
-puts myInventory
-puts myInventory.remove(@lemons, 1)
+Game.new
--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,44 @@
 class Player
 
   def initialize
+    @cash = 100
+    @lemons = 2
+    @sugar = 2
+    @ice = 2
+    @pitchers = 0
+  end
+
+  def print_status
+    puts "*" * 50
+    puts "Cash: $#{@cash}"
+    puts "Lemons: #{@lemons} lemons"
+    puts "Sugar: #{@sugar} cups"
+    puts "Ice: #{@ice} bags"
+    puts "Pitchers of lemonade: #{@pitchers}"
+    puts "*" * 50
+  end
+
+  def has_cash?
+    @cash > 0
+  end
+
+  def make_lemonade(lemons, sugar, ice)
+    if @lemons >= lemons && @sugar >= sugar && @ice >= ice
+      @pitchers += 1
+      @lemons -= lemons
+      @sugar -= sugar
+      @ice -= ice
+      puts "You made one pitcher of lemonade!"
+      print_status
+    else
+      puts "You need #{lemons} lemons, #{sugar} cups of sugar, and #{ice} bags of ice to make a pitcher of lemonade."
+    end
+  end
+
+  def buy_lemons(n)
+    @lemons += n
+    @cash -= (5 * n)
+    puts "You bought #{n} lemons for $#{n * 5}."
   end
 
 end
